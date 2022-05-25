41daee100426d7dff458d948dba372b275e2862b
Tejas <tejast06@gmail.com>
2022-05-25 11:40:09 +0530

    Impact Guru

---
+++ src/test/java/TestFundRaiser.java
@@ -0,0 +1,52 @@
+import base.TestBase;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.StartANewFundRaiser;
+import testdata.TestDataImpactGuru;
+
+import java.io.IOException;
+
+public class TestFundRaiser extends TestBase implements TestDataImpactGuru {
+
+    public TestFundRaiser() throws IOException {
+    }
+
+    public HomePage homePage;
+    public StartANewFundRaiser startANewFundRaiser;
+
+    @BeforeTest
+    public void setUp() throws IOException {
+        initialization();
+        homePage = new HomePage();
+    }
+
+    @Test
+    public void createFundRaiser() throws IOException, InterruptedException {
+
+        //step1
+        startANewFundRaiser = homePage.clickFundRaiser();
+
+        //step2
+        startANewFundRaiser.selectIamRaisingFundsFor(medical);
+        Thread.sleep(3000);
+
+        //step3
+        startANewFundRaiser.enterDetails(name,emailid,number);
+
+        //step4
+        startANewFundRaiser.whoAreYouRaisingFundFor(parent);
+
+        //step5
+        startANewFundRaiser.setPatientDetails(name,age,male,number,city);
+
+
+
+
+
+
+
+
+
+    }
+}
